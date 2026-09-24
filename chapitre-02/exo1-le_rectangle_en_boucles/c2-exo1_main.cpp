8cd0bbf97af0d6b4d82a6fdd091cce3918e8bb57
doriane02 <abegadoriane@gmail.com>
2026-09-24 06:47:14 +0200

    Update c2-exo1_main.cpp

--- chapitre-02/exo1-le_rectangle_en_boucles/c2-exo1_main.cpp
+++ chapitre-02/exo1-le_rectangle_en_boucles/c2-exo1_main.cpp
@@ -2,11 +2,9 @@
 
 int main(){
 
-    int i,j;
+      for (int  i=0; i<7; i++ ){
 
-      for ( i=0; i<7; i++ ){
-
-        for( j=0; j<20; j++ ){
+        for( int j=0; j<20; j++ ){
 
           if ( i==0 || i==6 || j==0 || j==19 ){
             printf("#");
